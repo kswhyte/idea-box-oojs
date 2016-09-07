168d476f04f7896b66baec86d4539eae9c7bf925
Gabi Procell <gprocell927@gmail.com>
2016-09-07 17:14:33 -0600

    ideas saving to an array

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,26 +1,47 @@
+var ideaStore = [];
+
 $('#save-button').on('click', function() {
   var titleInput = $('#title-input').val();
   var bodyInput = $('#body-input').val();
-  saveIdea(titleInput, bodyInput);
+  generateNewIdea(titleInput,bodyInput);
 });
 
 $('#body-input').keypress(function(event) {
   if (event.which == 13) {
     var titleInput = $('#title-input').val();
     var bodyInput = $('#body-input').val();
-    saveIdea(titleInput, bodyInput);
   }
 });
 
-function saveIdea(titleInput, bodyInput) {
-  var newIdea = '<li><h3> class="idea-title"' + titleInput + '</h3>' + '<button class="remove-idea">x</button><break><p class="body-input">' + bodyInput + '</p><break>';
-  addIdeaToList(newIdea);
-  $('#title-input').focus();
+function getIdeaInput () {
+  var titleInput = $('#title-input').val();
+  var bodyInput = $('#body-input').val();
+  sendIdeasToArray(titleInput,bodyInput);
 }
 
-function addIdeaToList(newIdea) {
-  $('.idea-list').prepend(newIdea);
+function Idea(title, body) {
+  this.title = title;
+  this.body = body;
+  this.id = Date.now();
+  this.quality = 'swill';
 }
+
+function generateNewIdea(titleInput, bodyInput) {
+  var idea = new Idea(titleInput,bodyInput);
+  ideaStore.unshift(idea);
+  renderIdeaToPage(idea);
+}
+
+function renderIdeaToPage(idea) {
+  $('.idea-list').prepend('<li><h3 class="idea-title">' + idea.title + '</h3>' + '<button class="remove-idea">x</button><break><p class="body-input">' + idea.body + 'quality' + idea.quality'</p><break>');
+}
+
+//TODO: Store the array in local storage
+// get the array out of local storage on page load
+// write any ideas in the array to page on page load
+
+
+
 
 //
 // $('.linked-list').on('click', '.new-url-link', markAsRead);
