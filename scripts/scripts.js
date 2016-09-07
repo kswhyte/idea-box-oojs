b0d3cd0c7ff5029643edbc53cd3ccb469a5b77e7
Gabi Procell <gprocell927@gmail.com>
2016-09-07 17:59:13 -0600

    Saves idea to local storage

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,23 +1,4 @@
-var ideaStore = [];
-
-$('#save-button').on('click', function() {
-  var titleInput = $('#title-input').val();
-  var bodyInput = $('#body-input').val();
-  generateNewIdea(titleInput,bodyInput);
-});
-
-$('#body-input').keypress(function(event) {
-  if (event.which == 13) {
-    var titleInput = $('#title-input').val();
-    var bodyInput = $('#body-input').val();
-  }
-});
-
-function getIdeaInput () {
-  var titleInput = $('#title-input').val();
-  var bodyInput = $('#body-input').val();
-  sendIdeasToArray(titleInput,bodyInput);
-}
+var ideasList = [];
 
 function Idea(title, body) {
   this.title = title;
@@ -26,28 +7,40 @@ function Idea(title, body) {
   this.quality = 'swill';
 }
 
+$('#save-button').on('click', function() {
+  var titleInput = $('#title-input').val();
+  var bodyInput = $('#body-input').val();
+  generateNewIdea(titleInput, bodyInput);
+});
+
+$('#body-input').keypress(function(event) {
+  if (event.which == 13) {
+    var titleInput = $('#title-input').val();
+    var bodyInput = $('#body-input').val();
+    generateNewIdea(titleInput, bodyInput);
+  }
+});
+
+function getIdeaInput() {
+  var titleInput = $('#title-input').val();
+  var bodyInput = $('#body-input').val();
+  storeIdea(titleInput, bodyInput);
+}
+
+function storeIdea(titleInput, bodyInput) {
+  localStorage.setItem('ideasList', JSON.stringify(ideas));
+}
+
 function generateNewIdea(titleInput, bodyInput) {
-  var idea = new Idea(titleInput,bodyInput);
-  ideaStore.unshift(idea);
+  var idea = new Idea(titleInput, bodyInput);
+  ideasList.unshift(idea);
   renderIdeaToPage(idea);
 }
 
 function renderIdeaToPage(idea) {
-  $('.idea-list').prepend('<li><h3 class="idea-title">' + idea.title + '</h3>' + '<button class="remove-idea">x</button><break><p class="body-input">' + idea.body + 'quality' + idea.quality'</p><break>');
+  $('.idea-list').prepend('<li><h3 class="idea-title">' + idea.title + '</h3>' + '<button class="remove-idea"> x </button><break><p class="body-input">' + idea.body + '</p><break><section class="vote"><img id="upvote" src="./images/svg-images/upvote.svg" /><img id="down-vote" src="./images/svg-images/downvote.svg" /><p>quality:</p>' + idea.quality + '</section><break>');
 }
 
-//TODO: Store the array in local storage
+//To DO:
 // get the array out of local storage on page load
-// write any ideas in the array to page on page load
-
-
-
-
-//
-// $('.linked-list').on('click', '.new-url-link', markAsRead);
-//
-// function addToLinkList(siteLink) {
-//   $('.linked-list').append(siteLink);
-//   clearFields();
-//   countTotals();
-// }
+// write any ideas in the array to page on load
